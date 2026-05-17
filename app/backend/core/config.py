c2082a8cdf5f7026558c51d584627a375bb5dcf7
= <=>
2026-05-18 00:51:00 +0530

    updated core/config

--- app/backend/core/config.py
+++ app/backend/core/config.py
@@ -5,12 +5,4 @@ from pydantic_settings import BaseSettings
 
 load_dotenv()
 
-GROQ_API_KEY = os.getenv("GROK_API_KEY")
-PSQL_PASSWORD = os.getenv("PSQL_PASSWORD")
-
-class JwtConfig(BaseSettings):
-    SECRET: str = os.getenv("JWT_SECRET")
-    ALGO: str = "HS256"
-    EXPIRE_MIN: int = 180
-
-jwtconfig = JwtConfig()
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
