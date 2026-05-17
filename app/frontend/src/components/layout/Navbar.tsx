9b3d1f7991f91b5fcc9c20365188f149a50d2243
= <=>
2026-05-18 00:40:15 +0530

    updated logo

--- app/frontend/src/components/layout/Navbar.tsx
+++ app/frontend/src/components/layout/Navbar.tsx
@@ -1,8 +1,10 @@
+import Logo from "../../assets/logo.png";
+
 export default function Navbar() {
     return (<>
         <div className="flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-300 bg-[#FFF] relative transition-all w-full">
             <a href="/" className="flex flex-row items-center gap-3">
-                <img className="h-10" src="https://cdn-icons-png.flaticon.com/128/18627/18627360.png" alt="logo" />
+                <img className="h-10" src={Logo} alt="logo" />
                 <p className="text-lg font-medium text-[#132440]">Ai Quiz Generator</p>
             </a>
            
