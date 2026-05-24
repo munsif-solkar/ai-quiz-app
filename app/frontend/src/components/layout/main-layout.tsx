3b201d92a17f37f6c6a8c228ba1519e5e693a7c2
= <=>
2026-05-25 00:28:52 +0530

    fixed main layout width issue

--- app/frontend/src/components/layout/main-layout.tsx
+++ app/frontend/src/components/layout/main-layout.tsx
@@ -6,7 +6,7 @@ export default function MainLayout({children}:{children:React.ReactNode}){
     return (
         <div className="flex min-h-screen flex-col justify-center items-center ">
             <Navbar/>
-            <main className="flex-1 container flex flex-col justify-center items-center  w-full">
+            <main className="flex-1 container flex flex-col justify-center items-center w-full">
             {children}
             </main>
         </div>
