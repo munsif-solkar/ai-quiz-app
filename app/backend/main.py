90718376596d5073713dc6b765e7fdf6961a876f
Munsif Solkar <79749427+munsif-solkar@users.noreply.github.com>
2026-08-28 20:58:54 +0530

    Update allow_origin_regex for CORS settings

--- app/backend/main.py
+++ app/backend/main.py
@@ -27,7 +27,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-    allow_origin_regex=r'http://.*:5173'
+    allow_origin_regex=r"https://.*\.onrender\.com"
 )
 
 @app.exception_handler(RequestValidationError)
